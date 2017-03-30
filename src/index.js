1a4e807db0efc830578ff25b1f88b089adcbb006
Johan Girod <johangirod@gmail.com>
2017-03-30 17:09:07 +0200

    withcharles

--- src/index.js
+++ src/index.js
@@ -1,41 +1,19 @@
-const { xprod, repeat } = require('ramda');
+import {xprod, range} from "ramda";
 
-const createGrid = (n) => {
-    const grid = repeat(false, n);
-    return grid.map(() => repeat(false, n));
+export const createGrid = n => {
+    const grid = new Array(n);
+    grid.fill(new Array(n));
+    return grid;
 };
 
-const populate = arr => arr.map(line => line.map(() => !!Math.round(Math.random())));
+export const nextState = grid => {
+    const coordinates = xprod(range(0, grid.length), range(0, grid.length));
+    coordinates.forEach(([x, y]) => {
+        grid[x][y] =
+        (grid[x][y] && grid[x][y-1] && grid[x][y+1] && grid[x][y-1] && grid[x][y+1])||
+        (grid[x][y] && grid[x-1][y] && grid[x+1][y] && grid[x-1][y] && grid[x+1][y]);
 
-const computeNeighboursNumber = (state, i, j) => {
-    const neighboursCoordinate = xprod([j - 1, j, j + 1], [i - 1, i, i + 1])
-        .filter(([x, y]) => x > 0 && y > 0 && x < state.length && y < state.length)
-        .filter(([x, y]) => !(x === i && j === i));
-    return neighboursCoordinate.reduce((sum, [x, y]) => state[x][y] + sum, 0);
+    });
+
+    return grid;
 };
-
-const computeNextState = (state) => {
-    const nextState = createGrid(state.length);
-    for (let i = 0; i < state.length; i++) {
-        for (let j = 0; j < state.length; j++) {
-            const neighbourNumber = computeNeighboursNumber(state, i, j);
-            nextState[i][j] =
-                neighbourNumber === 3 ||
-                (nextState[i][j] === 1 && neighbourNumber === 2);
-        }
-    }
-    return nextState;
-};
-
-const loop = n => {
-    let currentState = populate(createGrid(10));
-    for (let i = 0; i < n; i++) {
-        currentState = computeNextState(currentState);
-        console.log(currentState);
-        console.log('===\n');
-    }
-    return currentState;
-};
-
-
-loop(12);
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,28 @@
 /* eslint-env jest */
-import twelve from './index';
+import { createGrid, nextState } from './index';
 
-describe('index', () => {
-    it('should be 12', () => {
-        expect(twelve).toBe(12);
+describe('createGrid', () => {
+    it('should initialized a nxn grid', () => {
+        expect(createGrid(10)).toHaveLength(10);
+        expect(createGrid(10)[0]).toHaveLength(10);
+        expect(createGrid(10)[9]).toHaveLength(10);
+    });
+});
+
+describe('nextState', () => {
+    test('a single cell should die immediatly', () => {
+        expect(nextState([[true]])).toEqual([[false]]);
+    });
+
+    test('a single dead cell should remain dead', () => {
+        expect(nextState([[false]])).toEqual([[false]]);
+    });
+
+    test('a living cell arround 2 living cell in a row stay alive', () => {
+        expect(nextState([[true, true, true]])[0][1]).toBe(true);
+    });
+
+    test('a living cell arround 2 living cell in a column stay alive', () => {
+        expect(nextState([[true], [true], [true]])[1][0]).toBe(true);
     });
 });
